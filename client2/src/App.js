2d409977add5119a29ffa2a88a99dd5cf621e50a
Snedker <wsnedker@eav1052490.eac.ad.ea.com>
2018-11-05 11:06:38 -0800

    aasdlfjsdal

---
+++ client2/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage.js';
+import Fib from './Fib.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">Home</Link>
+            <Link to="/otherPage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherPage" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
